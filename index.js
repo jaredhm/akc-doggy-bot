2cb85f40587d509e54b23d49b0f3ec5b2de44f7a
Jared MacFarlane <jmacfarlane@athenahealth.com>
2018-03-09 13:33:31 -0500

    Revert "first commit"
    This reverts commit 526264843c2714c77e8ef6037898c4273a258fd7.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => res.render('pages/index'))
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
