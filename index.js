385be2f2db783d08b6c0e0f1725fea9a00b78727
Jared MacFarlane <jmacfarlane@athenahealth.com>
2018-03-12 21:51:07 -0400

    adding auth page

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+const auth = require('./lib/auth');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('Foo!');
+});
+
+app.get('/auth', function(req, res) {
+  req.status(200).send(auth.getAuthPage());
 });
 
 app.listen(PORT, function () { console.log(`Listening on ${ PORT }`) });
